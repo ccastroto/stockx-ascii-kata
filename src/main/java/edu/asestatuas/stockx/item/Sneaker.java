ad5dadb69d26bcf574989c5674773a559e26c182
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-03-20 14:18:17 +0100

    feat(sneaker):  Añadir la clase sneaker

--- src/main/java/edu/asestatuas/stockx/item/Sneaker.java
+++ src/main/java/edu/asestatuas/stockx/item/Sneaker.java
@@ -1,4 +1,36 @@
 package edu.asestatuas.stockx.item;
 
-public class Sneaker {
+public class Sneaker implements Item {
+
+    private String name;
+    private int sale;
+    private int ask;
+    private int bid;
+
+    public Sneaker(String id, String sneaker) {
+        this.name = sneaker;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getSale(){
+        return this.sale;
+    }
+
+    @Override
+    public int getAsk(){
+        return this.ask;
+    }
+
+    @Override
+    public int getBid(){
+        return this.bid;
+    }
+
+    public String toString() {
+        return this.name;
+    }
 }
