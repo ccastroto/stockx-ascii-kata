10bb211026e2756bbf684c011c0d77fb61917bdd
avedado <mariaalonsoteis@gmail.com>
2025-03-21 13:32:56 +0100

    refactor(sneaker): refactirizar test de obtener el id de la zapatilla

--- src/test/java/edu/asestatuas/SneakerTest.java
+++ src/test/java/edu/asestatuas/SneakerTest.java
@@ -9,10 +9,16 @@ public class SneakerTest {
     @Test
     public void getSneakerTest() {
         // ARRANGE  // APPLY
-        Sneaker shoe = new Sneaker("Jordan");
+        Sneaker shoe = new Sneaker("555088-105", "Jordan 1 Retro High Dark Mocha");
 
         // TEST
-        assertEquals("Jordan", shoe.getName());
+        assertEquals("Jordan 1 Retro High Dark Mocha", shoe.getName());
+    }
+
+    @Test
+    public void getStyleTest(){
+        Sneaker shoe = new Sneaker("555088-105", "Jordan 1 Retro High Dark Mocha");
+        assertEquals("555088-105", shoe.getStyle());
     }
 
 }
