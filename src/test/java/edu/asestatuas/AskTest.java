a08b8d6f79999291ceea7252745eef0a94827f1f
ccastroto <carloscastroaldrover@gmail.com>
2025-03-26 09:33:39 +0100

    test(ask): Añadir los casos test del Ask

---
+++ src/test/java/edu/asestatuas/AskTest.java
@@ -0,0 +1,29 @@
+package edu.asestatuas;
+
+import edu.asestatuas.stockx.item.*;
+import org.junit.Test;
+import java.util.Optional;
+import static org.junit.Assert.*;
+
+public class AskTest {
+
+    @Test
+    public void SizeAskTest(){
+        Ask ask = new Ask("13", 288);
+        assertEquals("13", ask.size());
+        assertEquals(Optional.of(288), Optional.ofNullable(ask.ask()));
+    }
+
+    @Test
+    public void ValueTest(){
+        Ask ask = new Ask("13", 288);
+        assertNotNull(String.valueOf(288), ask.ask());
+    }
+
+    @Test
+    public  void toStringTest(){
+        Ask ask = new Ask("13", 288);
+        assertNotNull(String.valueOf(ask), ask.toString());
+        assertNotNull(ask.toString());
+    }
+}
